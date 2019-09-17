8c0b0c57a2f7a652a67eb8bb6a8d3fdd9746bad9
Ricardo de A Paula <ricardo.de.alpa@gmail.com>
2019-09-17 07:33:06 -0300

    Testando a primeira classe de dominio - Categoria

---
+++ src/main/java/br/com/playbackparamissa/cursomc/domain/Categoria.java
@@ -0,0 +1,115 @@
+package br.com.playbackparamissa.cursomc.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String descricao;
+	private Integer codigo;
+	private Integer codigoPai;
+	private Integer pontos;
+	private Integer limiteQuantidade;
+	private Integer limitePontos;	
+	
+	public Categoria() {
+		
+	}
+
+	public Categoria(Integer id, String descricao) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public Integer getCodigoPai() {
+		return codigoPai;
+	}
+
+	public void setCodigoPai(Integer codigoPai) {
+		this.codigoPai = codigoPai;
+	}
+
+	public Integer getPontos() {
+		return pontos;
+	}
+
+	public void setPontos(Integer pontos) {
+		this.pontos = pontos;
+	}
+
+	public Integer getLimiteQuantidade() {
+		return limiteQuantidade;
+	}
+
+	public void setLimiteQuantidade(Integer limiteQuantidade) {
+		this.limiteQuantidade = limiteQuantidade;
+	}
+
+	public Integer getLimitePontos() {
+		return limitePontos;
+	}
+
+	public void setLimitePontos(Integer limitePontos) {
+		this.limitePontos = limitePontos;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+	
+}
--- src/main/java/br/com/playbackparamissa/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/playbackparamissa/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,42 @@
 package br.com.playbackparamissa.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.playbackparamissa.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Capacitação profissional com carga horária de 15 a 30 horas");
+		Categoria cat2 = new Categoria(2, "Capacitação profissional com carga horária de 31 a 80 horas");
+		Categoria cat3 = new Categoria(3, "Capacitação profissional com carga horária a partir de 81 horas");
+		Categoria cat4 = new Categoria(4, "Participação Institucional");
+		Categoria cat5 = new Categoria(5, "Produção científica/técnica apresentação/publicação de artigos e monografia");
+		Categoria cat6 = new Categoria(6, "Produção científica/técnica apresentação/publicação de dissertações");
+		Categoria cat7 = new Categoria(7, "Produção científica/técnica apresentação/publicação de teses");
+		Categoria cat8 = new Categoria(8, "Capacitação profissional com carga horária inferior a 15 horas");
+		Categoria cat9 = new Categoria(9, "Participação em bancas de trabalhos academicos/monitoria/instrutoria");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		lista.add(cat3);
+		lista.add(cat4);
+		lista.add(cat5);
+		lista.add(cat6);
+		lista.add(cat7);
+		lista.add(cat8);
+		lista.add(cat9);
+		
+		return lista;
 		
 	}
 
